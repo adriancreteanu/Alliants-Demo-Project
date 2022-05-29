802b2efd0360ea6daf81115f23a04e26cb600387
Adrian Creteanu <creteanu.adrian@gmail.com>
2022-05-30 00:56:29 +0300

    Map opening hours from response and leave TODO

--- Alliants Demo Project/View Controllers/Vendors List /Model/Vendor.swift
+++ Alliants Demo Project/View Controllers/Vendors List /Model/Vendor.swift
@@ -9,17 +9,41 @@ import Foundation
 
 struct Vendor: Decodable {
     let displayName: String
-    let heroImage: VendorHeroImage
+    let heroImage: HeroImage
     let description: String
+    let openingHours: OpeningHours?
     
     enum CodingKeys: String, CodingKey {
         case displayName = "display_name"
         case heroImage = "hero_image"
+        case openingHours = "opening_hours"
         case description
     }
 }
 
-
-struct VendorHeroImage: Decodable {
+// MARK: - HeroImage
+struct HeroImage: Decodable {
     let url: String
+}
+
+
+// MARK: - OpeningHours
+struct OpeningHours: Codable {
+    let id: Int
+    let monday, tuesday, wednesday, thursday: [Day]
+    let friday, saturday, sunday: [Day]
+}
+
+// MARK: - Day
+struct Day: Codable {
+    let id: Int
+    let opensAt, closesAt: String?
+    let closesLate: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case opensAt = "opens_at"
+        case closesAt = "closes_at"
+        case closesLate = "closes_late"
+    }
 }
--- Alliants Demo Project/View Controllers/Vendors List /View/VendorViewData.swift
+++ Alliants Demo Project/View Controllers/Vendors List /View/VendorViewData.swift
@@ -11,10 +11,21 @@ struct VendorViewData {
     let name: String
     let description: String
     let imageURL: URL?
+    let isOpen: Bool
 
     init(vendor: Vendor) {
         self.name = vendor.displayName
         self.description = vendor.description
         self.imageURL = URL(string: vendor.heroImage.url)
+        self.isOpen = VendorViewData.isOpen(vendor: vendor)
+    }
+    
+    static func isOpen(vendor: Vendor) -> Bool {
+        // TODO: Steps to check if vendor is open or closed
+        // 1. See if we have a value for opening hours. If not, return false.
+        // 2. Look for the current day in the opening hours object.
+        // 3. If current time is between `opensAt` and `closesAt`, return true. Else, return false.
+        
+        return true
     }
 }
