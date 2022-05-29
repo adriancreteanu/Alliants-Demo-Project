9c5571bbbbc58f9bfb6d9be5f49c33cf4e14bdf5
Adrian Creteanu <creteanu.adrian@gmail.com>
2022-05-29 20:06:34 +0300

    Create UI for vendors list cell

--- Alliants Demo Project/View Controllers/Vendors List /VendorsListViewController.swift
+++ Alliants Demo Project/View Controllers/Vendors List /VendorsListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  VendorsListViewController.swift
 //  Alliants Demo Project
 //
 //  Created by Adrian Creteanu on 29.05.2022.
@@ -7,13 +7,46 @@
 
 import UIKit
 
-class VendorsListViewController: UIViewController {
-
+class VendorsListViewController: UITableViewController {
+    // MARK: - Properties
+    
+    private var activityIndicator: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.hidesWhenStopped = true
+        return spinner
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setup()
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        navigationItem.title = "Vendors"
+    }
+    
+    private func setup() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let cellNib = UINib(nibName: "VendorTableViewCell", bundle: nil)
+        tableView.register(cellNib, forCellReuseIdentifier: VendorTableViewCell.identifier)
+        tableView.backgroundView = activityIndicator
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: VendorTableViewCell.identifier, for: indexPath) as? VendorTableViewCell else {
+            fatalError("Dequeued cell is of incorrect type.")
+        }
+        
+        cell.configure()
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
 }
-
---
+++ Alliants Demo Project/View Controllers/Vendors List /View/VendorTableViewCell/VendorTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RepoTableViewCell.swift
+//  Alliants Demo Project
+//
+//  Created by Adrian Creteanu on 29.05.2022.
+//
+
+import UIKit
+
+class VendorTableViewCell: UITableViewCell {
+    
+    static let identifier: String = "VendorCell"
+    
+    @IBOutlet var vendorImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var availabilityLabel: UILabel!
+    
+    // MARK: - Setup
+    
+    func configure() {
+        vendorImageView.backgroundColor = .blue
+    }
+}
