92eb7c0380b88695a554a12eceade6f0b1d08960
Adrian Creteanu <creteanu.adrian@gmail.com>
2022-05-29 18:21:15 +0300

    Project Setup

---
+++ Alliants Demo Project/View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alliants Demo Project
+//
+//  Created by Adrian Creteanu on 29.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
